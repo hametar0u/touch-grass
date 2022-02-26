d7fbddcd68d459f3452bfce3f70badc959e36ddd
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-02-26 13:46:05 -0800

    Animation loads on page

--- contentScript.js
+++ contentScript.js
@@ -6,6 +6,13 @@ if (typeof init === 'undefined') {
         injectElement.innerHTML = 'Hello this is reminder to touch grass';
         document.body.insertBefore(injectElement, document.body.firstChild);
         // document.body.appendChild(injectElement);
+        var iframe  = document.createElement ('iframe');
+        iframe.src  = chrome.runtime.getURL ('/animation.html');
+        iframe.style.position = "absolute";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.zIndex = "100";
+        document.body.insertBefore(iframe, document.body.firstChild);
     }
     init();
 }
