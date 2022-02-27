d9bdf12d1e5f5acf6c543a25ead6280e5036356f
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-02-26 17:46:38 -0800

    Tracks time persistently
    starting from when the component is first loaded

--- src/background.js
+++ src/background.js
@@ -1,12 +1,26 @@
-try {
 
+try {
+    const start = new Date();
+    var value = start.getTime();
+    chrome.storage.local.set({ 'time': value }, function() {
+        console.log('Value is set to ' + value);
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         if (changeInfo.status == 'complete') {
+            console.log("executing");
+            let startTime;
+            const now = new Date();
+            chrome.storage.local.get(['time'], function(result){
+                startTime = result.time;
+                console.log("Start time: ", result.time);
+                let deltatime = now.getTime() - startTime;
+                console.log(deltatime);
+            });
             chrome.scripting.executeScript({
                 files: ['contentScript.js'],
                 target: {tabId: tab.id}
             }); 
         }
+    });
     });
 } catch (e) {
     console.log(e);
